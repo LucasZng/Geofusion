d66ebfede065e5bb0c661d875fffb5d6af98563d
LucasZng <lucasoliveira194@gmail.com>
2023-02-07 20:14:24 -0300

    commit

--- handler.py
+++ handler.py
@@ -2,17 +2,17 @@ import pickle
 import pandas as pd
 from flask             import Flask, request, Response
 from geofusion.Geofusion import Geofusion
-import umap.umap_ as umap
+#import umap.umap_ as umap
 
 # loading model
 model_faturamento = pickle.load(open('model/regression.pkl', 'rb'))
 model_potencial = pickle.load(open('model/classifier.pkl', 'rb'))
-model_rf_etaria = pickle.load(open('model/clust_rf_etaria.pkl', 'rb'))
-model_clust_etaria = pickle.load(open('model/clust_etaria.pkl', 'rb'))
-model_clust_classe = pickle.load(open('model/clust_classe.pkl', 'rb'))
+#model_rf_etaria = pickle.load(open('model/clust_rf_etaria.pkl', 'rb'))
+#model_clust_etaria = pickle.load(open('model/clust_etaria.pkl', 'rb'))
+#model_clust_classe = pickle.load(open('model/clust_classe.pkl', 'rb'))
 
-cols_etaria = [['popAte9', 'popDe10a14', 'popDe15a19', 'popDe20a24', 'popDe25a34', 'popDe35a49', 'popDe50a59', 'popMaisDe60']]
-cols_classe = [['domiciliosA1', 'domiciliosA2', 'domiciliosB1', 'domiciliosB2', 'domiciliosC1', 'domiciliosC2', 'domiciliosD', 'domiciliosE']]
+#cols_etaria = [['popAte9', 'popDe10a14', 'popDe15a19', 'popDe20a24', 'popDe25a34', 'popDe35a49', 'popDe50a59', 'popMaisDe60']]
+#cols_classe = [['domiciliosA1', 'domiciliosA2', 'domiciliosB1', 'domiciliosB2', 'domiciliosC1', 'domiciliosC2', 'domiciliosD', 'domiciliosE']]
 
 # initialize API
 app = Flask( __name__ )
@@ -43,22 +43,22 @@ def geofusion_predict():
         # potencial
         df_potencial = pipeline.get_prediction_potencial(model_potencial, test_raw, df3)
         
-        # df_tre
-        df_tree = pipeline.get_tree(model_rf_etaria, df3[cols_etaria])
-        
-        # df_umap
-        df_umap = pipeline.get_umap(df3[cols_classe])
-        
-        # etaria
-        df_etaria = pipeline.get_cluster(model_clust_etaria, df_tree, test_raw, 3, 'etaria')
-        
-        # classe
-        df_classe = pipeline.get_cluster(model_clust_classe, df_umap, test_raw, 5, 'classe')
+        ## df_tre
+        #df_tree = pipeline.get_tree(model_rf_etaria, df3[cols_etaria])
+        #
+        ## df_umap
+        #df_umap = pipeline.get_umap(df3[cols_classe])
+        #
+        ## etaria
+        #df_etaria = pipeline.get_cluster(model_clust_etaria, df_tree, test_raw, 3, 'etaria')
+        #
+        ## classe
+        #df_classe = pipeline.get_cluster(model_clust_classe, df_umap, test_raw, 5, 'classe')
         
         df_response = df_faturamento.copy()
         df_responde['pred_potencial'] = df_potencial['pred_potencial']
-        df_responde['etaria'] = df_etaria['etaria']
-        df_responde['classe'] = df_classe['classe']
+        #df_responde['etaria'] = df_etaria['etaria']
+        #df_responde['classe'] = df_classe['classe']
         
         return df_response
     
